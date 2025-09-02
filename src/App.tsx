b0a2a135e2c2b5f3d8df47cf1fe011df102cd726
Gironmv12 <gironsan08918@gmail.com>
2025-09-01 21:03:19 -0600

    Configuracion inicial del proyecto

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Hola mundo</h1>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </div>
+      
+    </>
+  );
+}
+
+export default App;
