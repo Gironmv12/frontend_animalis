66200221a0d15e6f4344890e787e8d595b24fb2a
Gironmv12 <gironsan08918@gmail.com>
2025-09-07 12:36:33 -0600

    corrección del término 'produccion' a 'production' en la configuración de axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    import.meta.env.MODE === "produccion"
+    import.meta.env.MODE === "production"
       ? import.meta.env.VITE_API_URL_PROD
       : import.meta.env.VITE_API_URL,
   headers: {
