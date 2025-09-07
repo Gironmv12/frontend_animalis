11e98a43d93e54c53328f446abd5876187f3a7fe
Gironmv12 <gironsan08918@gmail.com>
2025-09-07 12:15:00 -0600

    despliegue url produccion

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? import.meta.env.REACT_APP_API_URL
+      : import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
